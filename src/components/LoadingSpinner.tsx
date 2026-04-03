306c0b73222f2fcc49bb8251d6c320d3f66c3f6b
Siddhant097 <siddhantmukund0628@gmail.com>
2026-04-03 13:53:11 +0530

    Deploy finance dashboard to GitHub Pages

--- src/components/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export const LoadingSpinner = () => {
-  return (
-    <div className="flex min-h-[30vh] items-center justify-center">
-      <div className="h-9 w-9 animate-spin rounded-full border-4 border-slate-200 border-t-sky-500 dark:border-slate-700 dark:border-t-sky-400" />
-    </div>
-  );
-};
